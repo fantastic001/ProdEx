2de0ed1607abbd42b9e0b4239d06b3f425e90f6f
Stefan Nožinić <stefan@lugons.org>
2019-06-15 18:12:57 +0200

    User widget view imitial implementation

--- frontend/App.vue
+++ frontend/App.vue
@@ -1,5 +1,20 @@
+<script>
+
+import WidgetUserSingle from "./src/user/widget-user-single.vue";
+
+export default {
+  name: "App",
+  components: {
+    "WidgetUserSingle": WidgetUserSingle
+  }
+}
+
+</script>
+
+
 <template>
   <div>
     <h1>Hello World!</h1>
+    <WidgetUserSingle id="0" />
   </div>
 </template>
---
+++ frontend/src/user/widget-user-single.vue
@@ -0,0 +1,25 @@
+<script>
+import UserService from "./service";
+
+export default {
+    name: "WidgetUserSingle",
+    props: ["id"],
+    data: function () {
+        return UserService.get(this.id);
+    },
+}
+</script>>
+
+<template>
+    <div class="widget-user-single"> 
+        <p>{{ firstname }} {{ lastname }}</p>
+        <p>{{ email}} </p>
+    </div>
+
+</template>
+
+<style scoped> 
+
+
+
+</style>
