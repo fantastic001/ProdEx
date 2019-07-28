3ef6cc0ac8f6f8cb42d18c258fd64ba79e712026
Stefan Nožinić <stefan@lugons.org>
2019-07-29 00:13:20 +0200

    Adding frontend side for user registration

--- frontend/App.vue
+++ frontend/App.vue
@@ -1,6 +1,7 @@
 <script>
 
 import WidgetUserMulti from "./src/widgets/User/WidgetUserMulti.vue";
+import WidgetUserNew from "./src/widgets/User/WidgetUserNew.vue";
 import AdminDetail from "./src/views/AdminDetail.vue";
 import BuyerDetail from "./src/views/BuyerDetail.vue";
 import SellerDetail from "./src/views/SellerDetail.vue";
@@ -8,6 +9,7 @@ export default {
   name: "App",
   components: {
     "WidgetUserMulti": WidgetUserMulti,
+    "WidgetUserNew": WidgetUserNew,
     "AdminDetail": AdminDetail,
     "BuyerDetail": BuyerDetail,
     "SellerDetail": SellerDetail,
@@ -23,6 +25,7 @@ export default {
     <BuyerDetail buyer="0" />
     <SellerDetail seller="0" />
     <WidgetUserMulti />
+    <WidgetUserNew />
  </div>
 
 </template>
