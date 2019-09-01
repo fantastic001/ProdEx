13edcc8e40ccf5665e3d36c0d61d5d1cdd962845
Stefan Nožinić <stefan@lugons.org>
2019-09-01 14:56:27 +0200

    Change scrriptts to be compatible to new templater version

--- doc/templates/data/make-components.sh
+++ doc/templates/data/make-components.sh
@@ -3,5 +3,5 @@ rm -rf example
 mkdir example
 
 for i in Order User Buyer Admin Seller City Item Category OrderReview BuyerFavoriteItem Comment Message Conversation FromAdminMessage ToAdminMessage ItemReport SellerReport BuyerSellerLike; do
-	templater ___component___.java example/$i.java ../$i.json
+	templater ___component___.java example/ ../$i.json
 done
--- doc/templates/services/make-components.sh
+++ doc/templates/services/make-components.sh
@@ -3,5 +3,5 @@ rm -rf example
 mkdir example
 
 for i in Order User Buyer Admin Seller City Item Category OrderReview BuyerFavoriteItem Comment Message Conversation FromAdminMessage ToAdminMessage ItemReport SellerReport BuyerSellerLike; do
-	templater ___component___Service.java example/${i}Service.java ../${i}.json
+	templater ___component___Service.java example/ ../${i}.json
 done
