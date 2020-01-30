53750cff149de84ad72cf0d5bc6d1e9dcceeb4da
Stefan Nožinić (server) <stefan@lugons.org>
2020-01-30 23:39:41 +0100

    load auth data from local storage on refresh

--- frontend/App.vue
+++ frontend/App.vue
@@ -5,8 +5,8 @@ export default {
   data: function () {
 	return {
 		data: {
-			user: {},
-			role: "NOT_LOGGED"
+			user: localStorage.getItem("user"),
+			role: localStorage.getItem("role")
 		}
 	}
   },
